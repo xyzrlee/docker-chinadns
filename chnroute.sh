8cfbdcb9165bcad43c87c431ed2b50354fa4853b
Ricky Li <cnrickylee@gmail.com>
2019-08-22 09:53:26 +0800

    reorg

--- chnroute.sh
+++ chnroute.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' >/usr/local/share/chnroute.txt
+curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }'
